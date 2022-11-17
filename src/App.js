5b3a0da48cc7c7a0e4ec38c8ac6ce996fab7cd80
Leo Pacheco Mac <leo@tobydev.com>
2022-11-16 22:04:39 -0500

    Change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Home from "./pages/home/Home";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Chat from "./pages/chat/Chat";
 import Footer from "./components/Footer";
